31895f089cc023ae1b5e27d3d38f287366022e1a
adamsimsy <adamsimsy@gmail.com>
2019-06-09 17:23:39 +1000

    allowed example mvc website run on http

--- src/ExampleWebApp/Startup.cs
+++ src/ExampleWebApp/Startup.cs
@@ -49,7 +49,7 @@ namespace ExampleWebApp
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
